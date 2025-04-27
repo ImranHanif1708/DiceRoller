89815e9190bc344e18d2fd3de180b85cdbf8291c
ImranHanif1708 <Imranhanif1708@gmail.com>
2025-04-27 11:34:59 +0500

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Label
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerTheme {
+               DiceRollerApp()
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DiceRollerApp(){
+
+    DiceWithButtonAndImage()
+
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)) {
+
+    val diceCounts = remember { mutableStateListOf(0,0,0,0,0,0) }
+    var totalrolls by remember { mutableStateOf(0) }
+
+    var result by remember { mutableStateOf(1) }
+    var imageResource = when(result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+
+    Column(modifier = modifier
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("Total Rolls = $totalrolls")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            for (i in 1..3){
+                Text("No.$i : ${diceCounts[i - 1]}")
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            for (i in 4..6){
+                Text("No.$i : ${diceCounts[i - 1]}")
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = imageResource.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            result = (1..6).random()
+            totalrolls++
+            diceCounts[result - 1]++}, Modifier.height(48.dp).width(100.dp)) {
+            Text(text = stringResource(R.string.roll))
+        }
+
+    }
+}
+
